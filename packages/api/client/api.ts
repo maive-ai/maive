8893a914ba3a751b7e2284f64d2972fd854ca7d7
willcray <will@willcray.com>
2025-10-22 16:43:52 -0700

    fix: regenerated api

--- packages/api/client/api.ts
+++ packages/api/client/api.ts
@@ -13,15 +13,15 @@
  */
 
 
-import type { AxiosInstance, AxiosPromise, RawAxiosRequestConfig } from 'axios';
-import globalAxios from 'axios';
 import type { Configuration } from './configuration';
+import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
+import globalAxios from 'axios';
 // Some imports not used depending on template conditions
 // @ts-ignore
+import { DUMMY_BASE_URL, assertParamExists, setApiKeyToObject, setBasicAuthToObject, setBearerAuthToObject, setOAuthToObject, setSearchParams, serializeDataIfNeeded, toPathString, createRequestFunction } from './common';
 import type { RequestArgs } from './base';
-import { DUMMY_BASE_URL, assertParamExists, createRequestFunction, serializeDataIfNeeded, setBearerAuthToObject, setSearchParams, toPathString } from './common';
 // @ts-ignore
-import { BASE_PATH, BaseAPI, RequiredError, operationServerMap } from './base';
+import { BASE_PATH, COLLECTION_FORMATS, BaseAPI, RequiredError, operationServerMap } from './base';
 
 /**
  * Provider-agnostic analysis data from completed calls.
@@ -94,7 +94,7 @@ export interface AuthResponse {
 export const CRMProvider = {
     ServiceTitan: 'service_titan',
     JobNimbus: 'job_nimbus',
-    MockCrm: 'mock'
+    Mock: 'mock'
 } as const;
 
 export type CRMProvider = typeof CRMProvider[keyof typeof CRMProvider];
