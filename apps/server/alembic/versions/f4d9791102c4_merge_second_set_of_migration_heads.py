92f43a02f303a863c8b9ef57fe60dd33cb37a851
willcray <will@willcray.com>
2025-11-19 11:52:57 -0800

    fix: formatting

--- apps/server/alembic/versions/f4d9791102c4_merge_second_set_of_migration_heads.py
+++ apps/server/alembic/versions/f4d9791102c4_merge_second_set_of_migration_heads.py
@@ -5,15 +5,16 @@ Revises: 500e69adfc16, d7e7b31d766f
 Create Date: 2025-11-19 11:50:33.525854
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'f4d9791102c4'
-down_revision: Union[str, Sequence[str], None] = ('500e69adfc16', 'd7e7b31d766f')
+revision: str = "f4d9791102c4"
+down_revision: Union[str, Sequence[str], None] = ("500e69adfc16", "d7e7b31d766f")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
